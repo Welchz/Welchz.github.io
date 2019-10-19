5aabd592b7479b0b71c9f4eb22fe6f7a24a4306d
Mitchell <MitchellThomas.101@gmail.com>
2019-10-19 16:18:50 -0600

    small changes

---
+++ Week5/js/active.js
@@ -0,0 +1,13 @@
+var navContainer = document.getElementById("nav");
+
+// Get all buttons with class="btn" inside the container
+var navs = navContainer.getElementsByClassName("navBar");
+
+// Loop through the buttons and add the active class to the current/clicked button
+for (var i = 0; i < navs.length; i++) {
+  navs[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace("active", "");
+    this.className += "active";
+  });
+}
