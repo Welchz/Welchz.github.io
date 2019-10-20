5f8aad4147bd4304d5d03478cb4c896772aaa255
Mitchell <MitchellThomas.101@gmail.com>
2019-10-19 23:47:32 -0600

    fixed all problems and made pancake alert

--- Week5/js/active.js
+++ Week5/js/active.js
@@ -1,6 +1,6 @@
-var navContainer = document.getElementById("nav");
+/*var navContainer = document.getElementById("nav");
 
-// Get all buttons with class="btn" inside the container
+// Get all buttons with class="navBar" inside the container
 var navs = navContainer.getElementsByClassName("navBar");
 
 // Loop through the buttons and add the active class to the current/clicked button
@@ -10,4 +10,33 @@ for (var i = 0; i < navs.length; i++) {
     current[0].className = current[0].className.replace("active", "");
     this.className += "active";
   });
+}*/
+
+function toggleMenu() {
+    // get the menu
+    var menu = document.getElementById('nav');
+
+    // get the current value of the menu's display property
+    var displaySetting = menu.style.display;
+
+    // now toggle the menu, depending on current state
+    if (displaySetting == 'block') {
+      // menu is visible. hide it
+      menu.style.display = 'none';
+
+    }
+    else {
+      // menu is hidden. show it
+      menu.style.display = 'block';
+    }
+  }
+function pancakeAlert(){
+var date = new Date();
+var alert = document.getElementById("pancake_alert");
+if (date.getDay() == 5) {
+      alert.style.display = "block";
+  }
+else {
+      alert.style.display = "none";
+  }
 }
